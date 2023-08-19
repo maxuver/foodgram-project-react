31a70e8e9fe0656059c881cd3e0b7c8fa560f03c
Maxim Patsyuk <maxuverv@gmail.com>
2023-08-19 15:56:09 +0400

    push9

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -18,5 +18,5 @@ class Command(BaseCommand):
         ) as file:
             reader = csv.DictReader(file)
             Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)  # ss
-        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
+                Ingredient(**data) for data in reader)  # sss
+        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!')) 
