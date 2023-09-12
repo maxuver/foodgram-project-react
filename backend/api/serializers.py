013779014ddc10f6e9b387222b1e61d9995c140b
Maxim Patsyuk <maxuverv@gmail.com>
2023-09-12 14:42:58 +0400

    Add RecipeShortSerializer, SubscribeSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,10 @@
 from users.models import User, Subscribe
 from djoser.serializers import UserSerializer
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, Tag, Recipe
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
+from drf_extra_fields.fields import Base64ImageField
+from rest_framework import status, exceptions
 
 
 class CustomUserSerializer(UserSerializer):
@@ -25,6 +27,57 @@ class CustomUserSerializer(UserSerializer):
                 and bool(Subscribe.objects.filter(user=user, author=obj)))
 
 
+class SubscribeSerializer(CustomUserSerializer):
+    recipes_count = SerializerMethodField()
+    recipes = SerializerMethodField()
+
+    class Meta(CustomUserSerializer.Meta):
+        fields = CustomUserSerializer.Meta.fields + (
+            'recipes_count', 'recipes'
+        )
+        read_only_fields = ('email', 'username')
+
+    def validate(self, data):
+        author = self.instance
+        user = self.context.get('request').user
+        if Subscribe.objects.filter(author=author, user=user).exists():
+            raise exceptions.ValidationError(
+                detail='Нельзя оформить подписку повторно!',
+                code=status.HTTP_400_BAD_REQUEST
+            )
+        if user == author:
+            raise exceptions.ValidationError(
+                detail='Невозможно подписаться на самого себя!',
+                code=status.HTTP_400_BAD_REQUEST
+            )
+        return data
+
+    def get_recipes_count(self, obj):
+        return obj.recipes.count()
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        recipes = obj.recipes.all()
+        if limit:
+            recipes = recipes[:int(limit)]
+        serializer = RecipeShortSerializer(recipes, many=True, read_only=True)
+        return serializer.data
+
+
+class RecipeShortSerializer(ModelSerializer):
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time'
+        )
+
+
 class IngredientSerializer(ModelSerializer):
     class Meta:
         model = Ingredient
@@ -35,3 +88,4 @@ class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
