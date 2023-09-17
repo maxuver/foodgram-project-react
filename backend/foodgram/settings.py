c5b9690ad0de6f2b450855c7592d3d192d58feee
Maxim Patsyuk <maxuverv@gmail.com>
2023-09-17 19:07:30 +0400

    Prod fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', ' ')
 
 DEBUG = int(os.getenv('DEBUG', '1'))
 
-ALLOWED_HOSTS = ['158.160.76.223', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
