ad7255101bcef428c6b923437ff2aabd97f8aee0
Maxim Patsyuk <maxuverv@gmail.com>
2023-09-24 12:32:40 +0400

    Push

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -108,9 +108,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_ROOT = '/var/html/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/html/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
